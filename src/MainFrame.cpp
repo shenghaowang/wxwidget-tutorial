b8f73499029ea5e4b78786a50e85c71ad1248c36
Shenghao1993 <wangshenghao1993@gmail.com>
2024-01-20 12:13:07 +0800

    Stylise control widgets

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -8,37 +8,43 @@ MainFrame::MainFrame(const wxString &title)
   wxButton *button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50),
                                   wxSize(100, 35));
 
-  wxCheckBox *checkBox =
-      new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 55));
+  wxCheckBox *checkBox = new wxCheckBox(
+      panel, wxID_ANY, "CheckBox", wxPoint(550, 55), wxDefaultSize,
+      wxCHK_3STATE | wxCHK_ALLOW_3RD_STATE_FOR_USER);
 
   wxStaticText *staticText = new wxStaticText(
-      panel, wxID_ANY, "StaticText - NOT editable", wxPoint(120, 150));
+      panel, wxID_ANY, "StaticText - NOT editable", wxPoint(0, 150),
+      wxSize(400, -1), wxALIGN_CENTER_HORIZONTAL);
+  staticText->SetBackgroundColour(*wxLIGHT_GREY);
 
-  wxTextCtrl *textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable",
-                                        wxPoint(500, 145), wxSize(200, -1));
+  wxTextCtrl *textCtrl =
+      new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145),
+                     wxSize(200, -1), wxTE_PASSWORD);
 
-  wxSlider *slider = new wxSlider(panel, wxID_ANY, 25, 0, 100,
-                                  wxPoint(100, 250), wxSize(200, -1));
+  wxSlider *slider =
+      new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250),
+                   wxSize(200, -1), wxSL_VALUE_LABEL);
 
-  wxGauge *gauge =
-      new wxGauge(panel, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
+  wxGauge *gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(590, 205),
+                               wxSize(-1, 125), wxGA_VERTICAL);
   gauge->SetValue(50);
 
   wxArrayString choices;
+  choices.Add("Item C");
   choices.Add("Item A");
   choices.Add("Item B");
-  choices.Add("Item C");
 
   wxChoice *choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375),
-                                  wxSize(100, -1), choices);
+                                  wxSize(100, -1), choices, wxCB_SORT);
   choice->Select(0);
 
-  wxSpinCtrl *spinCtrl =
-      new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(500, 375), wxSize(100, -1));
+  wxSpinCtrl *spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(500, 375),
+                                        wxSize(100, -1), wxSP_WRAP);
 
   wxListBox *listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475),
-                                     wxSize(100, -1), choices);
+                                     wxSize(100, -1), choices, wxLB_MULTIPLE);
 
-  wxRadioBox *radioBox = new wxRadioBox(
-      panel, wxID_ANY, "RadioBox", wxPoint(485, 475), wxDefaultSize, choices);
+  wxRadioBox *radioBox =
+      new wxRadioBox(panel, wxID_ANY, "RadioBox", wxPoint(555, 450),
+                     wxDefaultSize, choices, 3, wxRA_SPECIFY_ROWS);
 }
